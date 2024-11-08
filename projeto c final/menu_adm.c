c299ba2b9c87b01bc175d565df4f83582e9515ab
VictorFerraretto <victor260406@gmail.com>
2024-11-08 13:55:27 -0300

    Create menu_adm.c

---
+++ projeto c final/menu_adm.c
@@ -0,0 +1,51 @@
+void menu(){
+
+int cont;
+
+  while(1){
+    printf("\n");
+    printf("\n");
+    printf("Bem vindo ao menu!\n");
+    printf("O que você deseja acessar?\n");
+    printf("--------------------------\n");
+    printf("1-Cadastrar novo investidor\n");
+    printf("2-Excluir investidor\n");
+    printf("3-Cadastrar nova criptomoeda\n");
+    printf("4-Excluir criptomoeda\n");
+    printf("5-Consultar saldo de um investidor\n");
+    printf("6-Consultar extrado de um investidor\n");
+    printf("7-Atualizar cotação\n");
+    printf("8-Sair\n");
+    scanf("%d", &cont);
+    printf("\n");
+    return cont;
+    scanf("%d", &cont);
+    printf("\n");
+
+
+      switch (cont){
+      case 1:
+        printf("...\n");
+        break;
+      case 2:
+        printf("...\n");
+        break;
+      case 3:
+        printf("...\n");
+        break;
+      case 4:
+        printf("...\n");
+        break;
+      case 5:
+        printf("...\n");
+        break;
+      case 6:
+        printf("...\n");
+        break;
+      case 7:
+        printf("...\n");
+      case 8:
+        printf("...\n");
+    }
+  }
+}
