806717a10e7febd7298d5d049795a86b541f6ca5
VictorFerraretto <victor260406@gmail.com>
2024-11-08 14:01:32 -0300

    Update menu_adm.c

--- projeto c final/menu_adm.c
+++ projeto c final/menu_adm.c
@@ -20,5 +20,30 @@ int cont;
     scanf("%d", &cont);
     printf("\n");
     return cont;
-  
+
 }
+
+ // switch (cont){
+ //      case 1:
+ //        printf("...\n");
+ //        break;
+ //      case 2:
+ //        printf("...\n");
+ //        break;
+ //      case 3:
+ //        printf("...\n");
+ //        break;
+ //      case 4:
+ //        printf("...\n");
+ //        break;
+ //      case 5:
+ //        printf("...\n");
+ //        break;
+ //      case 6:
+ //        printf("...\n");
+ //        break;
+ //      case 7:
+ //        printf("...\n");
+ //      case 8:
+ //        printf("...\n");
+ //    }
