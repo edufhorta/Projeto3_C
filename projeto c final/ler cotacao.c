abfea8c972944cd9ab27996ac095b6f4f68017c8
edufhorta <unifehorta@fei.edu.br>
2024-11-19 22:40:58 -0300

    Create ler cotacao.c

---
+++ projeto c final/ler cotacao.c
@@ -0,0 +1,20 @@
+void lerCotacoes(cotacao* cotacoes, int* num_cotacoes) {
+    FILE *arquivo_cotacoes;
+    char linha[200];
+
+    arquivo_cotacoes = fopen("cripto.txt", "r");
+
+
+    *num_cotacoes = 0; // Inicializa o número de cotações
+
+    while (fgets(linha, sizeof(linha), arquivo_cotacoes) && *num_cotacoes < MAX_MOEDAS) {
+        sscanf(linha, "%s %f %f %f", 
+               cotacoes[*num_cotacoes].nome, 
+               &cotacoes[*num_cotacoes].valor, 
+               &cotacoes[*num_cotacoes].cotven, 
+               &cotacoes[*num_cotacoes].cotcomp);
+        (*num_cotacoes)++;
+    }
+
+    fclose(arquivo_cotacoes);
+}
