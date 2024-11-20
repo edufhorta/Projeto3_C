fbd0b8384f90255cda3036e93a881dd3b847508d
edufhorta <unifehorta@fei.edu.br>
2024-11-19 22:55:53 -0300

    Update biblioteca.h

--- projeto c final/biblioteca.h
+++ projeto c final/biblioteca.h
@@ -26,22 +26,22 @@ typedef struct {
 void copiarCpfsParaCarteira(carteira* carteiras, char cpf_lista[NUM_OF_USERS][MAX_CPF_LENGTH], int num_usuarios)
 void lerCotacoes(cotacao* cotacoes, int* num_cotacoes)
 void iniciacao_arquivos(float *carteira, float *carteiraBit, float *carteiraEth, float *carteiraXrp, char *cpf_lista, char *senha_lista);
-void atualizacao_das_carteiras(float *carteira,float *carteiraBit,float *carteiraEth,float *carteiraXrp);
-void apagarCripto(cotacao *cripto);
-void mostrarSaldo(float *carteira, float *carteiraBit, float *carteiraEth,float *carteiraXrp, int *indice);
+void atualizacao_das_carteiras(float *carteira,carteira*carteiras);
+void apagarCripto(cotacao *cotacoes);
+void mostrarSaldo(float *carteira,carteira* carteiras, int *indice);
 int login_novo(int *indice, char *cpf, char *senha, char *cpf_lista[NUM_OF_USERS][12], char *senha_lista[NUM_OF_USERS][7]);
 void extrato( char *cpf);
-void atualizacao_das_carteiras(float *carteira,float *carteiraBit,float *carteiraEth,float *carteiraXrp);
-void depositar(float*carteira, float*carteiraBit, float*carteiraEth, float*carteiraXrp, const char *cpf, int *indice);
-void sacar(float *carteira, float *carteiraBit, float *carteiraEth, float *carteiraXrp, const char *senha, int *indice, char *cpf);
-void Comprar_criptomoedas(float *carteira, float *carteiraBit, float *carteiraEth, float *carteiraXrp, const char *senha,const char *cpf, int *indice, float *bit, float *eth, float *rip);
-void Vender_criptomoedas(float *carteira, float *carteiraBit, float *carteiraEth, float *carteiraXrp, const char *cpf, int *indice, float *bit, float *eth, float *rip);
+void atualizacao_das_carteiras(float *carteira,carteira* carteiras);
+void depositar(float*carteira, const char *cpf, int *indice);
+void sacar(float *carteira, const char *senha, int *indice, char *cpf);
+void Comprar_criptomoedas(float *carteira,carteira* carteiras, const char *senha,const char *cpf, int *indice);
+void Vender_criptomoedas(float *carteira, carteira* carteiras, const char *cpf, int *indice);
 void Atualiza_cotacao(const char *cripto);
 void mostrar_cotacoes(const char *cripto);
 void criacao_cripto(const char *cripto);
 int carrega_cotacao(const char *cripto, cotacao **cotacoes);
 void extrato_adm (char cpf_lista[NUM_OF_USERS][12], char senha_lista[NUM_OF_USERS][7]);
-void mostrarSaldo_adm(float *carteira, float *carteiraBit, float *carteiraEth,float *carteiraXrp, char cpf_lista[NUM_OF_USERS][12]);
+void mostrarSaldo_adm(float *carteira,carteira* carteiras, char cpf_lista[NUM_OF_USERS][12]);
 void cadastrar_inv(char cpf_lista[NUM_OF_USERS][12]);
 int menu();
 int menu_adm();
